cb41cc19ffd705905c26a47209fddcf94070dcde
Jenmar Alano <jenmaracantilado.alano@gmail.com>
2024-10-12 21:47:12 +0800

    partial

---
+++ admin/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page | Barangayy Information System</title>
+    <link rel = "stylesheet" href = "index.css"/>
+</head>
+<body>
+    <header class = "header">
+            <img src="../asset/image/logo/New Project.png" alt="">
+
+            <h1><span style = "color:#FCFAEE;">BARANGAY </span><span style = "color:#F5E402;">INFORMATION SYSTEM</span></h1>
+    </header>
+    
+    <main id = "main">
+
+        <form action="">
+
+            <h1><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="m13 16 5-4-5-4v3H4v2h9z"></path><path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"></path></svg></span>LOGIN</h1>
+
+            <div>
+            <label for=""><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path></svg></span>Username</label><br>
+            <input type="text"><br>
+
+            <label for=""><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg></span>Password</label><br>
+            <input type="Password"><br>
+
+          
+            <input type="checkbox" id = "checkbox"><label for="" id = "check_span">Show Password</label><br>
+          
+            <input type="submit" id = "submit"> 
+            </div>
+            
+        </form>
+        
+        <div>
+            <h4>All records should be stored in a 
+            secure and confidential.</h4>
+            <img src="../asset/image/background/admin_bg/image-removebg-preview 1.png" alt="">
+        </div>
+
+    </main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,14 +3,140 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home page | Barangayy Information System</title>
+    <link rel = "stylesheet" href = "index.css"/>
+
 </head>
 <body>
-    <h1>HELLO WORLD</h1>
+        <header class = "header">
+            <div class = "social_logo">
+                <a href=""><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" ><path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path></svg></a>
+                <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M21.593 7.203a2.506 2.506 0 0 0-1.762-1.766C18.265 5.007 12 5 12 5s-6.264-.007-7.831.404a2.56 2.56 0 0 0-1.766 1.778c-.413 1.566-.417 4.814-.417 4.814s-.004 3.264.406 4.814c.23.857.905 1.534 1.763 1.765 1.582.43 7.83.437 7.83.437s6.265.007 7.831-.403a2.515 2.515 0 0 0 1.767-1.763c.414-1.565.417-4.812.417-4.812s.02-3.265-.407-4.831zM9.996 15.005l.005-6 5.207 3.005-5.212 2.995z"></path></svg></a>
+                <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"></path></svg></a>
+           
+            </div>
+            <div class = "btn_e-barangay">
+                <a href=""><button>E-BARANGAY CERTIFICATION</button></a>
+            </div>
+        </header>
+        
+        <nav class = "nav">
+            <div class = "logo">
+                <img src="asset/image/logo/New Project.png" alt="">
+            </div>
+            <div class = "headings">
+                <h1>BARANGAY PALIPARAN II</h1><br>
+                <h3>CITY OF DASMARIÑAS</h3>
+            </div>
+            <div class = "nav_">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Officer</a>
+                <a href="">Hotlines</a>
+                <a href="">Services</a>
+            </div>
+        </nav>
 
-        <h2>JEnmar</h2>
+        <section id = "home">
+            <img src="asset/image/background/bg.jpg" alt="">
+            <h1><span >WELCOME TO</span> <br> <span style = "color:#F5E402;">BARANGAY PALIPARAN</span></h1>
+        </section>
 
-        <p>sadalsdkasda</p>
-    <h1>adkajsds</h1>
+        <section id = "services">
+
+        <div>
+            <a href=""><button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="M2.047 14.667a.992.992 0 0 0 .466.607l1.909 1.104v2.199a1 1 0 0 0 1 1h2.199l1.104 1.91a1.002 1.002 0 0 0 1.366.366L12 20.75l1.91 1.104a1.002 1.002 0 0 0 1.366-.366l1.103-1.909h2.199a1 1 0 0 0 1-1V16.38l1.909-1.104a.999.999 0 0 0 .366-1.366L20.75 12l1.104-1.909a1 1 0 0 0-.366-1.366l-1.909-1.104V5.422a1 1 0 0 0-1-1H16.38l-1.103-1.909a1.004 1.004 0 0 0-.607-.466.994.994 0 0 0-.759.1L12 3.25l-1.909-1.104a.998.998 0 0 0-1.366.365l-1.104 1.91H5.422a1 1 0 0 0-1 1V7.62L2.513 8.725a1.001 1.001 0 0 0-.365 1.366L3.251 12l-1.104 1.909a1.003 1.003 0 0 0-.1.758z"></path></svg></button>
+            </a>
+            <h4>BARANGAY CLEARANCE</h4>
+        </div>
+           
+        <div>
+            <a href=""><button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"transform: ;msFilter:;"><path d="M7 14.001h2v2H7z"></path><path d="M19 2h-8a2 2 0 0 0-2 2v6H5c-1.103 0-2 .897-2 2v9a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2zM5 20v-8h6v8H5zm9-12h-2V6h2v2zm4 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V6h2v2z"></path></svg>
+            </button></a>
+            <h4>BUSINESS PERMIT</h4>
+        </div>
+           
+        <div>
+        <a href=""><button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" transform: ;msFilter:;"><circle cx="6" cy="4" r="2"></circle><path d="M9 7H3a1 1 0 0 0-1 1v7h2v7h4v-7h2V8a1 1 0 0 0-1-1z"></path><circle cx="17" cy="4" r="2"></circle><path d="M20.21 7.73a1 1 0 0 0-1-.73h-4.5a1 1 0 0 0-1 .73L12 14h2l-1 4h2v4h4v-4h2l-1-4h2z"></path></svg></button></a>
+        <h4>BARANGAY INDIGENCY</h4>
+        </div>
+
+        </section>
+
+        <section id = "official">
+        <h1>BARANGAY OFFICIALS</h1>
+            <div class = "chairman">
+              
+                <img src="asset/image/official/images.png" alt="">
+            </div>
+
+            <div class = "_kagawad">
+                <img src="asset/image/official/images.png" alt="">
+                <img src="asset/image/official/images.png" alt="">
+                <img src="asset/image/official/images.png" alt="">
+            </div>
+
+            <div class = "_kagawad">
+                <img src="asset/image/official/images.png" alt="">
+                <img src="asset/image/official/images.png" alt="">
+                <img src="asset/image/official/images.png" alt="">
+                <img src="asset/image/official/images.png" alt="">
+            </div>
+        </section>
+
+        <section id = "online_request"> 
+            <h1>ONLINE BARANGAY <br>REQUEST</h1>
+            <a href=""><button>Let's Go</button></a>
+        </section>
+
+        <section id = "galllery">
+            <h1>GALLERY</h1>
+
+            <div class = "div_gallery">
+                <img src="asset/image/gallery/1.jpg" alt="">
+                <img src="asset/image/gallery/2.jpg" alt="">
+                <img src="asset/image/gallery/3.jpg" alt="">
+
+                <img src="asset/image/gallery/4.jpg" alt="">
+                <img src="asset/image/gallery/5.jpg" alt="">
+                <img src="asset/image/gallery/6.jpg" alt="">
+
+                <img src="asset/image/gallery/7.jpg" alt="">
+                <img src="asset/image/gallery/8.jpg" alt="">
+                <img src="asset/image/gallery/9.jpg" alt="">
+            </div>
+            <a href=""><button>LOAD MORE</button></a>
+        </section>
+
+        <section id = "hotlines">
+            
+
+        </section>
+
+        <section id = "maps">
+                
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3866.1402204626515!2d120.99125687414404!3d14.30328438428866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d673f808ef65%3A0xf1e2a9d39e40bb!2sPaliparan%202%2C%20Dasmari%C3%B1as%2C%20Cavite!5e0!3m2!1sen!2sph!4v1728217955989!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
+
+
+        <footer id = "footer">
+            <div class = "item1_footer">
+                <h1>ABOUT</h1>
+                <div class = "div_img">
+                    <img src="asset/image/logo/New Project.png" alt="">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolores similique hic enim odit sed error cumque, maiores suscipit inventor</p>
+                </div>
+            </div>
+            <div class = "item2_footer">
+                <h1>Get in Touch</h1>
+                <p><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="m20.487 17.14-4.065-3.696a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a.997.997 0 0 0-.085-1.39z"></path></svg></span>0951 385 6318</p>
+                <p><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="m20.487 17.14-4.065-3.696a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a.997.997 0 0 0-.085-1.39z"></path></svg></span>0951 385 6318</p>
+                <p><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="m9 6.882-7-3.5v13.236l7 3.5 6-3 7 3.5V7.382l-7-3.5-6 3zM15 15l-6 3V9l6-3v9z"></path></svg></span>Paliparan II, Dasmariñas, Philippines</p>
+                <p><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" transform: ;msFilter:;"><path d="m18.73 5.41-1.28 1L12 10.46 6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64z"></path></svg></span>Barangay.paliparanII@gmail.com</p>
+            </div>
+        </footer>
+
+   
 </body>
 </html>
