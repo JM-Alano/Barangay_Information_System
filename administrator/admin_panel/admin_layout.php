419a8ad2914c297742685d45ebe6c98b6fb5b174
Jenmar Alano <Jenmaracantilado.alano@gmail.com>
2025-03-29 22:16:07 +0800

    03/29/2025

--- administrator/admin_panel/admin_layout.php
+++ administrator/admin_panel/admin_layout.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile | Barangayy Information System</title>
     <link rel = "stylesheet" href = "style.css/mian.stylecss.css"/>
-    <link rel = "stylesheet" href = "style.css/dash_style.css"/>
+    <link rel = "stylesheet" href = "style.css/reposive_main.css"/>
     <link rel="icon" href="/BIS/favicon/favicon.ico" type="image/x-icon">
 </head>
 <body>
